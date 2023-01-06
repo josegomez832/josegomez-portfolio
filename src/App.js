5cf6e9265c8b00be313dd23614d499df40891ccf
josegomez832 <gomez.jose853@gmail.com>
2023-01-06 00:15:25 -0600

    netlify toml

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Header from './Layout/Header'
 import Home from './Pages/Home'
 import Project from "./Pages/Project";
 import './App.scss';
